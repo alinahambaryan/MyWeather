5cebd1a23216f33ade6d398454169b2b0026e80f
Alina <alina_hambaryan@yahoo.com>
2015-09-18 14:18:25 +0400

    Initial Commit

---
+++ MyWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWeather
+//
+//  Created by ALINA HAMBARYAN on 9/18/15.
+//  Copyright (c) 2015 ALINA HAMBARYAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyWeather/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyWeather
+//
+//  Created by ALINA HAMBARYAN on 9/18/15.
+//  Copyright (c) 2015 ALINA HAMBARYAN. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
